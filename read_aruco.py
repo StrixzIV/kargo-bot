2ddbfda0436cf72df1ef813aaae295c8e000d691
Jirayu Kaewsing <strixz.self@gmail.com>
2023-07-09 15:50:13 +0700

    show aruco pose estimation axis

--- read_aruco.py
+++ read_aruco.py
@@ -1,11 +1,15 @@
 import cv2
-from utils.aruco import process_aruco
+import pickle
+from utils.aruco import process_aruco, pose_estimation
 
 aruco_type = cv2.aruco.DICT_5X5_100
 aruco_dict = cv2.aruco.getPredefinedDictionary(aruco_type)
 aruco_params = cv2.aruco.DetectorParameters()
 
 cam_stream = cv2.VideoCapture(0)
+
+with open('./calibration_output/calibration.pkl', 'rb') as f:
+    (camera_matrix, distortion_coefficients) = pickle.load(f)
 
 while True:
 
@@ -14,9 +18,10 @@ while True:
     if not is_frame:
         continue
 
-    (ids, view_frame) = process_aruco(frame, aruco_dict, aruco_params)
+    # (ids, view_frame) = process_aruco(frame, aruco_dict, aruco_params)
+    view_frame = pose_estimation(frame, aruco_dict, aruco_params, camera_matrix, distortion_coefficients)
 
-    print(f'ArUco ID detected: {ids}')
+    # print(f'ArUco ID detected: {ids}')
     cv2.imshow('ArUco View', view_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):

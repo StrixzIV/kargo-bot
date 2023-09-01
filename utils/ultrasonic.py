689a54369f11c77fae24056244993b710fe446f5
Jirayu <jirayukaewsing48@gmail.com>
2023-09-01 15:22:55 +0700

    Create ultrasonic module

---
+++ utils/ultrasonic.py
@@ -0,0 +1,44 @@
+import time
+import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+
+(trig, echo) = (6, 27)
+
+gpio.setup(trig, gpio.OUT)
+gpio.setup(echo, gpio.IN)
+
+gpio.output(trig, gpio.LOW)
+print('Initializing ultrasonic sensor...')
+
+time.sleep(2)
+
+def get_distance() -> float:
+    
+    gpio.output(trig, True)
+ 
+    time.sleep(0.00001)
+    gpio.output(trig, False)
+ 
+    start = time.time()
+    stop = time.time()
+ 
+    while gpio.input(echo) == 0:
+        start = time.time()
+ 
+    while gpio.input(echo) == 1:
+        stop = time.time()
+ 
+    timedelta = stop - start
+    distance = round(timedelta * 17150, 2)
+    
+    time.sleep(0.01)
+    
+    return distance
+
+
+if __name__ == '__main__':
+    
+    while True:
+        distance = get_distance()
+        print(distance)

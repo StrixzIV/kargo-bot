b53612f7ac27367448ce61de0b7eee208a7d8b0f
Jirayu <jirayukaewsing48@gmail.com>
2023-09-04 19:06:42 +0700

    Add aruco feedback

--- read_aruco.py
+++ read_aruco.py
@@ -5,7 +5,8 @@ import RPi.GPIO as gpio
 
 from rich import print
 from utils.aruco import pose_estimation
-from utils.lane_follow import get_feedback_from_lane, power_left, power_right, in1, in2, in3, in4
+from utils.ultrasonic import get_distance
+from utils.lane_follow import get_feedback_from_lane, power_left, power_right, in1, in2, in3, in4, light_matrix
 
 aruco_type = cv2.aruco.DICT_5X5_100
 aruco_dict = cv2.aruco.getPredefinedDictionary(aruco_type)
@@ -18,76 +19,120 @@ cam_stream = cv2.VideoCapture(0)
 with open('./calibration_output/calibration.pkl', 'rb') as f:
     (camera_matrix, distortion_coefficients) = pickle.load(f)
 
-while True:
 
-    (is_frame, frame) = cam_stream.read()
+try:
+    while True:
 
-    if not is_frame:
-        continue
+        light_matrix.fill((255, 255, 255))
+        (is_frame, frame) = cam_stream.read()
 
-    detected_markers = pose_estimation(frame, aruco_dict, aruco_params, camera_matrix, distortion_coefficients)
-    print(detected_markers)
-    
-    if not detected_markers:
+        if not is_frame:
+            continue
         
-        get_feedback_from_lane(frame, True)
+        distance = get_distance()
+                    
+        print(f'Distance: {distance}')
         
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-        
-        continue
-        
-    if detected_markers[0]['id'] in {0, 1}:
-        
-        for _ in range(5):
-            (is_frame, feedback_frame) = cam_stream.read()
-            get_feedback_from_lane(feedback_frame)
-        
-        if target_station == 1:
+        if distance <= 50:
             
-            power_left.ChangeDutyCycle(50)
-            power_right.ChangeDutyCycle(50)
+            power_left.ChangeDutyCycle(0)
+            power_right.ChangeDutyCycle(0)
             
-            time.sleep(1.5)
+            light_matrix.fill((255, 0, 0))
+            time.sleep(1)
+            light_matrix.fill((255, 255, 255))
             
-            power_left.ChangeDutyCycle(50)
-            power_right.ChangeDutyCycle(30)
+            continue
             
-            time.sleep(5)
+
+        detected_markers = pose_estimation(frame, aruco_dict, aruco_params, camera_matrix, distortion_coefficients)
+        print(detected_markers)
+        
+        if not detected_markers:
+            
+            get_feedback_from_lane(frame, False)
+            
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                light_matrix.fill((0, 0, 0))
+                gpio.cleanup()
+                cam_stream.release()
+                cv2.destroyAllWindows()
+                break
+            
+            continue
+            
+        if detected_markers:
+            
+            print('Target found')
             
             for _ in range(5):
-                (is_frame, feedback_frame) = cam_stream.read()
-                get_feedback_from_lane(feedback_frame)
+                
+                if detected_markers[0]['rvecs']['pitch'] > 0:
+                    
+                    power_left.ChangeDutyCycle(10)
+                    power_right.ChangeDutyCycle(0)
+                    
+                    time.sleep(0.1)
+                
+                elif detected_markers[0]['rvecs']['pitch'] < 0:
+                    
+                    power_left.ChangeDutyCycle(0)
+                    power_right.ChangeDutyCycle(10)
+                    
+                    time.sleep(0.1)
+                    
+            print('Adjustment complete')
+                    
+            if target_station == 1:
+                
+                power_left.ChangeDutyCycle(50)
+                power_right.ChangeDutyCycle(50)
+                
+                time.sleep(1.5)
+                
+                power_left.ChangeDutyCycle(50)
+                power_right.ChangeDutyCycle(30)
+                
+                time.sleep(5)
+                
+                for _ in range(5):
+                    (is_frame, feedback_frame) = cam_stream.read()
+                    get_feedback_from_lane(feedback_frame)
+                
+                power_left.ChangeDutyCycle(0)
+                power_right.ChangeDutyCycle(0)
+                
+                break
+                
             
-            power_left.ChangeDutyCycle(0)
-            power_right.ChangeDutyCycle(0)
+            elif target_station == 2:
+                
+                power_left.ChangeDutyCycle(50)
+                power_right.ChangeDutyCycle(50)
+                
+                time.sleep(5)
+                
+                power_left.ChangeDutyCycle(0)
+                power_right.ChangeDutyCycle(0)
+                
+                break
+                
+            print(f'Found: #{detected_markers[0]["id"]}')
             
-            break
-            
-        
-        elif target_station == 2:
-            
-            power_left.ChangeDutyCycle(50)
-            power_right.ChangeDutyCycle(50)
-            
-            time.sleep(2.5)
-            
-            power_left.ChangeDutyCycle(0)
-            power_right.ChangeDutyCycle(0)
-            
-            break
-            
-        print(f'Found: #{detected_markers[0]["id"]}')
-        
-        gpio.output(in1, gpio.HIGH)
-        gpio.output(in2, gpio.LOW)
-        gpio.output(in3, gpio.HIGH)
-        gpio.output(in4, gpio.LOW)
+            gpio.output(in1, gpio.HIGH)
+            gpio.output(in2, gpio.LOW)
+            gpio.output(in3, gpio.HIGH)
+            gpio.output(in4, gpio.LOW)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-
-gpio.cleanup()
-cam_stream.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            light_matrix.fill((0, 0, 0))
+            gpio.cleanup()
+            cam_stream.release()
+            cv2.destroyAllWindows()
+            break
+        
+except:
+    light_matrix.fill((0, 0, 0))
+    gpio.cleanup()
+    cam_stream.release()
+    cv2.destroyAllWindows()
